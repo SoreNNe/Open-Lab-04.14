0d0845dbea3a036bab8bbe49d8c105b8736b95c3
SoreNNe <71930836+SoreNNe@users.noreply.github.com>
2020-11-12 00:55:33 +0100

    .

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +7,34 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            int najdlhsie = 0;
+            for (int l = 0; l < strings.Length; l++)
+            {
+                if (strings[l].Length > najdlhsie)
+                {
+                    najdlhsie = strings[l].Length;
+                }
+            }
+            for (int i = 1; i < najdlhsie + 4; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("*");
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.Write("* ");
+                Console.Write(strings[i]);
+                for (int s = 0; s < najdlhsie - strings[i].Length; s++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" *");
+            }
+            for (int i = 1; i < najdlhsie + 4; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("*");
         }
     }
 }
